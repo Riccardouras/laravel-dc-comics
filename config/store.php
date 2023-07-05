e0eb8b210e6d97f1f86fc3a11354fa7847c1c5a7
[Riccardo Uras] <[riccardo.uras3@gmail.com]>
2023-07-05 10:13:10 +0200

    ricomincio da capo

---
+++ config/store.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'advImage' => "resources/img/adv.jpg",
+    'someLinks' => [
+        [
+            "href" => "/",
+            "text" => "Home Page",
+        ],
+        [
+            "href" => "/otherpage",
+            "text" => "Other Page",
+        ],
+        [
+            "href" => "https://laravel.com/docs/9.x",
+            "text" => "Documentation",
+        ]
+    ],
+    'someOtherData' => [ 42, 37, 73],
+    "listaImg" => [
+        "resources/img/buy-comics-digital-comics.png",
+        "resources/img/buy-comics-merchandise.png",
+        "resources/img/buy-comics-shop-locator.png",
+        "resources/img/buy-comics-subscriptions.png",
+    ]
+];
