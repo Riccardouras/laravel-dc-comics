157dc592344fd2aeaaae50771a78d4fbcd14bc21
[Riccardo Uras] <[riccardo.uras3@gmail.com]>
2023-07-06 11:01:59 +0200

    plus

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -5,32 +5,32 @@
     @csrf
     <label for="title">Title:</label>
     <input  class="form-control"type="text" name="title" id="title" required>
-    <br>
+
     <label for="description">Description:</label>
     <textarea  class="form-control"name="description" id="description" required></textarea>
-    <br>
+
     <label for="thumb">Thumbnail:</label>
     <input  class="form-control"type="text" name="thumb" id="thumb" required>
-    <br>
+
     <label for="price">Price:</label>
     <input  class="form-control"type="text" name="price" id="price" required>
-    <br>
+
     <label for="series">Series:</label>
     <input  class="form-control"type="text" name="series" id="series" required>
-    <br>
+
     <label for="sale_date">Sale Date:</label>
     <input  class="form-control"type="date" name="sale_date" id="sale_date" required>
-    <br>
+
     <label for="type">Type:</label>
     <input  class="form-control"type="text" name="type" id="type" required>
-    <br>
+
     <label for="artists">Artists:</label>
     <input class="form-control" type="text" name="artists" id="artists" required>
-    <br>
+
     <label for="writers">Writers:</label>
     <input class="form-control" type="text" name="writers" id="writers" required>
-    <br>
 
-    <button type="submit">Save <a href="{{ route('comics.index') }}"></a></button>
+
+    <button type="submit" class="form-control mt-4 btn btn-primary">Save <a href="{{ route('comics.index') }}"></a></button>
 </form>
 @endsection
