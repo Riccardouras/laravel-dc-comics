b2c8330eb998ef29f8b62d4eada68d26a5310526
[Riccardo Uras] <[riccardo.uras3@gmail.com]>
2023-07-06 11:38:38 +0200

    migliorie grafiche

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -7,22 +7,29 @@
         
     </div>
 </div>
-<table>
-    <thead>
+<table class="table table-striped table-bordered">
+    <thead  class="thead-dark">
         <tr>
             <th>Title</th>
             <th>Description</th>
+            <th>Thumbnail</th>
+            <th>Price</th>
+            <th>Series</th>
+            <th>Sale date</th>
+            <th>Type</th>
+            <th></th>
+            <th></th>
         </tr>
     </thead>
     <tbody>
         <tr>
-            <td>{{ $comic->title }}</td>
+            <td><a href="{{route("comics.show", $comic->id)}}">{{$comic->title}}</a></td>
             <td>{{ $comic->description }}</td>
             <td> <img src={{"$comic->thumb"}} alt={{"$comic->title"}}></td>
             <td>{{ $comic->price }}</td>
             <td>{{ $comic->series }}</td>
             <td>{{ $comic->sale_date }}</td>
-            <td>{{ $comic->type }}</td> 
+            <td>{{ $comic->type }}</td>
             <td> <form action="{{ route('comics.destroy', $comic) }}" method="post">
                 @csrf
                 @method('DELETE')
@@ -30,7 +37,7 @@
             </form></td> 
             <td><button class="btn btn-warning"><a href="{{ route('comics.edit', $comic->id)}}">Edit</a></button></td>
         </tr>
-       
     </tbody>
 </table>
+
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,5 @@
 <header class="p-3 d-flex gap-4">
-    <img class="logo" src="{{ Vite::asset('resources/img/logo.png') }}" alt="Laravel Logo">
-    <span>Some links: </span>
+    <img class="logo" src="https://th.bing.com/th/id/R.ebcc5f96b3059f120e7b946560e78909?rik=alRWgHfcCFI0RQ&riu=http%3a%2f%2fww1.prweb.com%2fprfiles%2f2012%2f03%2f14%2f9284377%2fcomic-life-2-icon.png&ehk=UB7ggpqb0nBbICt0kWx6Swrv%2bdTS9jRYWFyh5%2fYMqTo%3d&risl=&pid=ImgRaw&r=0" alt="DC-Marvel">
     <a href="{{ route('welcome') }}">Homepage</a>
     <a href="{{ route('comics.index') }}">Our Comics</a>
     <a href="{{ route('comics.create') }}">Create a New Comic</a>
